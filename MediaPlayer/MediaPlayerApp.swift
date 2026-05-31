4128bad7dd6fea69586e877f0706a8e555b5a5c4
Andrey Pantyuhin <panandafog@gmail.com>
2026-05-31 11:56:55 +0300

    Initial Commit

---
+++ MediaPlayer/MediaPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerApp.swift
+//  MediaPlayer
+//
+//  Created by Andrey Pantyuhin on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediaPlayerTests/MediaPlayerTests.swift
@@ -0,0 +1,18 @@
+//
+//  MediaPlayerTests.swift
+//  MediaPlayerTests
+//
+//  Created by Andrey Pantyuhin on 31.05.2026.
+//
+
+import Testing
+
+struct MediaPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
